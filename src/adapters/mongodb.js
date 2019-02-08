306eba64fd7603bdc32864ea351354b80e691d8e
Marco Tarantino <taran.marco@gmail.com>
2019-02-08 22:09:25 +0100

    Create collections when calling the adapter

--- src/adapters/mongodb.js
+++ src/adapters/mongodb.js
@@ -19,17 +19,32 @@ const dbMaker = async ( host = 'localhost', port = '27017', user, password, dbNa
   })
 
   try {
+    await setup_collections(client_connect,dbName);
     const client = await client_connect();
     const db = client.db(dbName);
-    return { client, dbObj: db, ...setup_db_api(db) };
+    return { client, dbObj: db, ...setup_db_api(client, db) };
   } catch(e) {
     console.error(e.stack);
   }
 }
 
-const setup_db_api = dbObj => {
-  const db = {};
-  return db;
+// setup collection creates its client and destroys it later
+const setup_collections = async (client_connect, dbName) => {
+  try {
+    const client = await client_connect();
+    const db = client.db(dbName);
+    const streams = await db.createCollection('streams');
+    await streams.createIndex({name: 'text'}, {unique: true});
+    const messages = await db.createCollection('messages');
+    await messages.createIndex({timestamp: 1});
+    return client.close();
+  } catch (e) {
+    console.error(e.stack);
+  }
+}
+
+const setup_db_api = async (client, dbObj) => {
+  return {};
 }
 
 export { dbMaker };
